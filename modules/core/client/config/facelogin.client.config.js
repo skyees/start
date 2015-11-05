6153ca5595688ab4d0ec5735d47ca9e597c2e6e2
skyees <inforajkiran@gmail.com>
2015-11-05 15:23:10 +0530

    facebook appication id layout meta tags

--- modules/core/client/config/facelogin.client.config.js
+++ modules/core/client/config/facelogin.client.config.js
@@ -12,22 +12,5 @@ angular.module('core')
                 xfbml: true
             });
         }
-    ])
-    .run( function( $rootScope ) {
-        // Load the facebook SDK asynchronously
-        (function(){
-            // If we've already installed the SDK, we're done
-            if (document.getElementById('facebook-jssdk')) {return;}
-
-            // Get the first script element, which we'll use to find the parent node
-            var firstScriptElement = document.getElementsByTagName('script')[0];
-
-            // Create a new script element and set its id
-            var facebookJS = document.createElement('script');
-            facebookJS.id = 'facebook-jssdk';
-
-            // Insert the Facebook JS SDK into the DOM
-            firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
-        }());
-    });
+    ]);
 
