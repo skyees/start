dc8325c1c48196331a64b693dac0929fea29ef3b
skyees <inforajkiran@gmail.com>
2015-11-13 04:30:55 +0530

    fb post

--- modules/users/server/config/strategies/facebook.js
+++ modules/users/server/config/strategies/facebook.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
     function (req, accessToken, refreshToken, profile, done) {
       // Set the provider data and include tokens
       var providerData = profile._json;
-        var ifriends = JSON.stringify(providerData);
+        console.log(providerData);
       providerData.accessToken = accessToken;
       providerData.refreshToken = refreshToken;
         FB.setAccessToken('accessToken');
@@ -52,7 +52,6 @@ module.exports = function (config) {
         username: profile.username || generateUsername(profile),
         profileImageURL: (profile.id) ? '//graph.facebook.com/' + profile.id + '/picture?type=large' : undefined,
         provider: 'facebook',
-        friends:ifriends,
         providerIdentifierField:'id',
         providerData: providerData
       };
