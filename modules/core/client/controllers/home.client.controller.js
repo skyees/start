4b4f56536f151faaf24f057fe4a07040f8044e3a
skyees <inforajkiran@gmail.com>
2015-11-12 21:03:34 +0530

    fb post

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,8 +1,18 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-  function ($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication','$facebookProvider',
+  function ($scope, Authentication,$facebookProvider) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+      $scope.loadFriends = function() {
+
+          $facebookProvider.api('/me/friends', function(response) {
+              $scope.$apply(function() {
+                  $scope.myFriends = response.data;
+                  console.log($scope.myFriends);
+              });
+
+          });
+      };
   }
 ]);
