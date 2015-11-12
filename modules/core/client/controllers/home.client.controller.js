c58c273030343c0cb98782655edbfbf52e86d5b6
skyees <inforajkiran@gmail.com>
2015-11-12 21:51:40 +0530

    fb post

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication','$facebookProvider',
+angular.module('core').controller('HomeController', ['$scope','Authentication','$facebookProvider',
   function ($scope, Authentication,$facebookProvider) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
       $scope.loadFriends = function() {
-          $facebookProvider.api('/me/friends', function(response) {
+          $facebook.api('/me/friends', function(response) {
               $scope.$apply(function() {
                   $scope.myFriends = response.data;
                   console.log($scope.myFriends);
