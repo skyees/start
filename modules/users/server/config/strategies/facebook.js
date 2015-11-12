2b15216a30cdad3e7a81a93147cfd08153f2ecdb
skyees <inforajkiran@gmail.com>
2015-11-12 14:51:56 +0530

    fb post

--- modules/users/server/config/strategies/facebook.js
+++ modules/users/server/config/strategies/facebook.js
@@ -9,6 +9,15 @@ var passport = require('passport'),
 
 var FB = require('fb');
 
+FB.api('4', function (res) {
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+    console.log(res.id);
+    console.log(res.name);
+});
+
 module.exports = function (config) {
   // Use facebook strategy
   passport.use(new FacebookStrategy({
