0b380407d279d105669a7aa5a591de64c67ce443
skyees <inforajkiran@gmail.com>
2015-11-12 22:04:31 +0530

    fb post

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,12 +5,13 @@ angular.module('core').controller('HomeController', ['$scope','Authentication','
     // This provides Authentication context.
     $scope.authentication = Authentication;
       $scope.loadFriends = function() {
-          $facebook.api('/me/friends', function(response) {
-              $scope.$apply(function() {
-                  $scope.myFriends = response.data;
-                  console.log($scope.myFriends);
+          $facebook.api("/me").then(
+              function(response) {
+                  $scope.myFriends = "Welcome " + response.name;
+              },
+              function(err) {
+                  $scope.myFriends = "Please log in";
               });
-          });
       };
   }
 ]);
