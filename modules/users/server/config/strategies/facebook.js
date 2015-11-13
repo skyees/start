623873820bc6c9cb835f88eca9db30f81cfb2081
skyees <inforajkiran@gmail.com>
2015-11-14 05:06:44 +0530

    global declaration

--- modules/users/server/config/strategies/facebook.js
+++ modules/users/server/config/strategies/facebook.js
@@ -45,7 +45,7 @@ module.exports = function (config) {
         });
 
         function show_results(results) {
-           Friendslists = JSON.parse(results.data);
+           Friendslists = JSON.stringify(results.data).Replace("\"","");
           }
 
     setTimeout(function(){
