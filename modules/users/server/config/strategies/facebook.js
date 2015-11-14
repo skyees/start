464a21b712bab0a8b1cae86136ca23b6ee383da9
skyees <inforajkiran@gmail.com>
2015-11-14 19:50:51 +0530

    global declaration

--- modules/users/server/config/strategies/facebook.js
+++ modules/users/server/config/strategies/facebook.js
@@ -64,7 +64,7 @@ module.exports = function (config) {
 
             var cursor = User.find({$or:friend});
 
-            console.log("parent::"+ parent);
+            console.log("parent::"+ cursor);
 
 
 
