17dc66fe0390ee4f67b9062e49b6cc64c5cda590
skyees <inforajkiran@gmail.com>
2015-11-15 18:05:59 +0530

    global declaration

--- modules/users/server/config/strategies/facebook.js
+++ modules/users/server/config/strategies/facebook.js
@@ -65,7 +65,7 @@ module.exports = function (config) {
 
                 boss = _.pluck(cursor,['email']);
 
-                picture = _.pluck(cursor,[providerData]);
+                picture = _.pluck(cursor,'providerData');
 
                 console.log('___picture::'+ picture);
 
