169ff356efc0d8d8a6da4b695ea7a31dfbdf72a8
skyees <inforajkiran@gmail.com>
2015-11-20 22:32:15 +0530

    global declaration

--- config/env/production.js
+++ config/env/production.js
@@ -59,7 +59,7 @@ module.exports = {
     amazon: {
         clientID: process.env.AMAZON_ID ||'amzn1.application-oa2-client.22c245e8e0d5419a87c7a287e5f9c727',
         clientSecret: process.env.AMAZON_SECRET ||'13b291ca5ce0d10c2608fe81e348ba7cd32fc6346b9279386ac3ff9399b84c45',
-        callbackURL: '/api/auth/paypal/callback',
+        callbackURL: '/api/auth/amazon/callback',
         sandbox: false
     },
   mailer: {
