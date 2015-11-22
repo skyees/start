4b39a8d3e43148d987d64b91545af9ac977316bd
skyees <inforajkiran@gmail.com>
2015-11-22 19:24:15 +0530

    global declaration

--- modules/core/client/config/facelogin.client.config.js
+++ modules/core/client/config/facelogin.client.config.js
@@ -17,7 +17,7 @@ angular.module('core')
 
         var s3 = null;
         var clientId = 'amzn1.application-oa2-client.22c245e8e0d5419a87c7a287e5f9c727'; // client ID
-        var roleArn = 'arn:aws:iam::274437077325 >:role/smart';
+        var roleArn = 'arn:aws:iam::274437077325:role/smart';
 
         window.onAmazonLoginReady = function() {
             amazon.Login.setClientId(clientId); // set client ID
