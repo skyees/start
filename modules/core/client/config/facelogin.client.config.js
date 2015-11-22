38ed9bc0758869912de5493349b37cea32998b92
skyees <inforajkiran@gmail.com>
2015-11-22 19:10:53 +0530

    global declaration

--- modules/core/client/config/facelogin.client.config.js
+++ modules/core/client/config/facelogin.client.config.js
@@ -25,6 +25,7 @@ angular.module('core')
             document.getElementById('login').onclick = function() {
                 amazon.Login.authorize({scope: 'profile'}, function(response) {
                     if (!response.error) { // logged in
+                        alert(responce);
                         AWS.config.credentials = new AWS.WebIdentityCredentials({
                             RoleArn: roleArn,
                             ProviderId: 'www.amazon.com',
