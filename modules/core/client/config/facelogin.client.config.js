8ea8eac0964de17c99bceb52195a84c638276a4b
skyees <inforajkiran@gmail.com>
2015-11-23 01:59:24 +0530

    global declaration

--- modules/core/client/config/facelogin.client.config.js
+++ modules/core/client/config/facelogin.client.config.js
@@ -29,7 +29,7 @@ angular.module('core')
 
                         amazon.Login.retrieveProfile(response.access_token, function(response) {
 
-                            alert(JSON.stringify(response.profile.Name));
+                            alert(JSON.stringify(response));
                             alert(response.profile.Name);
 
                              console.log('You are now logged in.');
