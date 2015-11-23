d0d95d05588b41537e9d4edac97cbc633089bd0c
skyees <inforajkiran@gmail.com>
2015-11-23 06:12:36 +0530

    global declaration

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope','Authentication','$facebook',
-  function ($scope, Authentication,$facebook) {
+angular.module('core').controller('HomeController', ['$scope','Authentication','$facebook','$sce',
+  function ($scope, Authentication,$facebook,$sce) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
-      $scope.yourURL = 'http://www.amazon.in?_encoding=UTF8&tag=thesmartking-21';
+      $scope.yourURL = $sce.trustAsResourceUrl('http://www.amazon.in?_encoding=UTF8&tag=thesmartking-21');
 
       $scope.loadFriends = function() {
           $facebook.api("/864549880333392/friends").then(
