6eca740f3b422ca8dd92826ad7b72cc5754997d8
skyees <inforajkiran@gmail.com>
2015-11-23 05:57:59 +0530

    global declaration

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,6 +4,7 @@ angular.module('core').controller('HomeController', ['$scope','Authentication','
   function ($scope, Authentication,$facebook) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+      $scope.yourURL='http://www.amazon.in?_encoding=UTF8&tag=thesmartking-21';
       $scope.loadFriends = function() {
           $facebook.api("/864549880333392/friends").then(
               function(response) {
