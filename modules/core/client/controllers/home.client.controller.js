a967caa27bd3aa5bc2054043d4e03a0cc8129ea9
skyees <inforajkiran@gmail.com>
2015-11-23 06:17:19 +0530

    global declaration

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,7 +5,7 @@ angular.module('core').controller('HomeController', ['$scope','Authentication','
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
-      $scope.yourURL = $sce.trustAsResourceUrl('https://www.amazon.in/');
+      $scope.yourURL = $sce.trustAsResourceUrl('https://www.amazon.in/'+ "&output=embed");
 
       $scope.loadFriends = function() {
           $facebook.api("/864549880333392/friends").then(
