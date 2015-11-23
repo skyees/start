2569d16ab81259f7440a44a580199c8d17963857
skyees <inforajkiran@gmail.com>
2015-11-24 02:56:43 +0530

    global declaration

--- modules/core/client/config/facelogin.client.config.js
+++ modules/core/client/config/facelogin.client.config.js
@@ -25,18 +25,20 @@ angular.module('core')
         // Load the facebook SDK asynchronously
         (function(){
 
-            var clientId = 'amzn1.application-oa2-client.9d181b1955a94e7d82751a7df1c30a75'; // client ID
 
-            window.onAmazonLoginReady = function() {
-                amazon.Login.setClientId(clientId); // set client ID
-
-            };
 
             var a = document.createElement('script'); a.type = 'text/javascript';
             a.async = true; a.id = 'amazon-login-sdk';
             a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
 
             window.onload = function() {
+
+                var clientId = 'amzn1.application-oa2-client.9d181b1955a94e7d82751a7df1c30a75'; // client ID
+
+                window.onAmazonLoginReady = function() {
+                    amazon.Login.setClientId(clientId); // set client ID
+
+                };
 
                 document.getElementById('Login').onclick = function () {
                     var options = {scope: 'profile'};
