11fb5206da4c4a738da519fd9173d656d282ecd2
skyees <inforajkiran@gmail.com>
2015-11-24 04:31:09 +0530

    global declaration

--- modules/core/client/config/facelogin.client.config.js
+++ modules/core/client/config/facelogin.client.config.js
@@ -20,7 +20,7 @@ angular.module('core')
     ]).run( function( $rootScope ) {
 
         window.onAmazonLoginReady = function() {
-            amazon.Login.setClientId('amzn1.application-oa2-client.22c245e8e0d5419a87c7a287e5f9c727'); // set client ID
+            amazon.Login.setClientId('amzn1.application-oa2-client.9d181b1955a94e7d82751a7df1c30a75'); // set client ID
         };
         // Load the facebook SDK asynchronously
           (function(){
