bcf973c4849847888def964cc5041867bba010f7
skyees <inforajkiran@gmail.com>
2015-11-24 23:12:32 +0530

    global declaration

--- modules/core/client/config/facelogin.client.config.js
+++ modules/core/client/config/facelogin.client.config.js
@@ -13,7 +13,9 @@ angular.module('core')
 
             });
 
-
+            window.onAmazonLoginReady = function() {
+                amazon.Login.setClientId('amzn1.application-oa2-client.9d181b1955a94e7d82751a7df1c30a75');
+            };
 
 
         }
@@ -24,10 +26,6 @@ angular.module('core')
 
         // Load the facebook SDK asynchronously
           (function(){
-
-              window.onAmazonLoginReady = function() {
-                  amazon.Login.setClientId('amzn1.application-oa2-client.9d181b1955a94e7d82751a7df1c30a75');
-              };
 
             var a = document.createElement('script'); a.type = 'text/javascript';
             a.async = true; a.id = 'amazon-login-sdk';
