d80ad7816f39664b20105b74309fdc4c98403cd8
skyees <inforajkiran@gmail.com>
2015-11-25 05:16:10 +0530

    global declaration

--- modules/core/server/controllers/core.server.controller.js
+++ modules/core/server/controllers/core.server.controller.js
@@ -4,6 +4,25 @@ var amazon = require('amazon-product-api');
  * Render the main application page
  */
 exports.renderIndex = function (req, res) {
+    console.log('amazon::india');
+    amazon = amazon.createClient({
+        awsId: "AKIAJ7AGDJDFA3BM7XPA",
+        awsSecret: "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",
+        awsTag: "thesmartking-21"
+    });
+    amazon.itemSearch({
+        director: 'Quentin Tarantino',
+        actor: 'Samuel L. Jackson',
+        searchIndex: 'DVD',
+        audienceRating: 'R',
+        responseGroup: 'ItemAttributes,Offers,Images'
+    }).then(function(results){
+        console.log('amazon::'+results);
+        alert(results);
+    }).catch(function(err){
+        alert(err);
+        console.log('amazon::'+err);
+    });
   res.render('modules/core/server/views/index', {
     user: req.user || null
   });
